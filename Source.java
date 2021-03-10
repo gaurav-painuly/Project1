1cbf6e8c36303a4f48c98b04f924df6c8191db34
gaurav-painuly <painuly.gaurav@gmail.com>
2021-03-10 11:28:43 -0800

    First commit

---
+++ Source.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;                  //ArrayList library imported
+
+class Source {
+   public static void main(String[] args) {
+       ArrayList<Float> random = new ArrayList<Float>();
+random.add(2f);
+random.add(4f);
+random.add(5f);
+random.add(10f);
+random.add(99.9f);
+
+System.out.println(random.contains(5.0f));
+   }
+
+}
+
+
